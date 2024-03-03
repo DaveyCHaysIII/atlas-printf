f40292d05b94bd5a1fded50bf2fb338f0e553021
Davey <david.c.hays.iii@gmail.com>
2024-03-03 19:03:31 +0000

    fixed stringlen differs!

--- testing/main.c
+++ testing/main.c
@@ -3,9 +3,21 @@ int _printf(const char* format, ...);
 
 int main(void)
 {
-	char* s;
-
-	s = "hi";
-	_printf("%%");
-	return(0);
+	int len;
+	int len2;
+	unsigned int ui;
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	_printf("%d\n", 44);
+	printf("%d\n", 44);
+	printf("Length:[%d, %i]\n", len2, len2);
+        _printf("Negative:[%d]\n", -762534);
+       	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("double percent: %%\n");
+	printf("double percent: %%\n");
+	return (0);
 }
